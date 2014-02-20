a906fa7eeaae2c83ae207c35c6f69a9ec6fd6d7b
kagross <kagross@iu.edu>
2014-02-20 14:51:19 +0000

    Setting Original before Branching

--- ops-agenda.sh
+++ ops-agenda.sh
@@ -3,14 +3,8 @@
 # Written by Kyle Gross                       #
 ###############################################
 
-
 #Get the full word Value of today (ie Monday, Tuesday)
 TODAY=$(date "+%A" -d today)
-
-declare -i HOUR
-HOUR=$(date "+%H" -d today)
-
-
 
 # Decide if the operator needs to be next-week or next-monday depending on which day the cron job will run this
 # TOPIC will hold the Topic Name such as Minutes2010August02
@@ -19,48 +13,39 @@ HOUR=$(date "+%H" -d today)
 if [ $TODAY == "Monday" ]; then
         TOPIC=$(echo Minutes`date +%Y%B%d -d this-week`)
         MEETINGDATE=$(echo ---+ Operations Meeting `date "+%B %d, %Y" -d this-week`)
-        COUNT=1
 
 else
     	TOPIC=$(echo Minutes`date +%Y%B%d -d next-monday`)
         MEETINGDATE=$(echo ---+ Operations Meeting `date "+%B %d, %Y" -d next-monday`)
-        COUNT=2
 fi
 
-echo $MEETINGDATE
-echo $TOPIC
-echo $HOUR
-echo $COUNT
-
-
-
 # Uses template file to copy Topic into a temporary file to be edited then put into production
-#sudo cp /usr/local/twiki/data/Operations/OperationsMeetingTemplate.txt ./$TOPIC.tmp
+sudo cp /usr/local/twiki/data/Operations/OperationsMeetingTemplate.txt ./$TOPIC.tmp
 
 # Makes changes to date in template file and outputs to final topic.
-#sudo sed '2i\'"$MEETINGDATE" $TOPIC.tmp > $TOPIC.txt
+sudo sed '2i\'"$MEETINGDATE" $TOPIC.tmp > $TOPIC.txt
 
 # Cleans up temporary file
-#sudo rm $TOPIC.tmp
+sudo rm $TOPIC.tmp
 
 # Set permissions on topic so that TWiki can edit it.
-#sudo chown apache:apache $TOPIC.txt
+sudo chown apache:apache $TOPIC.txt
 
 # Moves topic from local user directory to twiki directory
-#sudo mv $TOPIC.txt /usr/local/twiki/data/Operations/
+sudo mv $TOPIC.txt /usr/local/twiki/data/Operations/
 
 # Confirms file that was created.  Not needed if put in crontab.
-#echo $TOPIC.txt created.
+echo $TOPIC.txt created.
 
 
 # Creates local temp file of Meeting Minutes list
-#sudo cp /usr/local/twiki/data/Operations/ProductionMeetingMinutes.txt ./ProductionMeetingMinutes.tmp
+sudo cp /usr/local/twiki/data/Operations/ProductionMeetingMinutes.txt ./ProductionMeetingMinutes.tmp
 
 # Inserts topic into correct spot in list
-#sudo sed '17i\'"   * $TOPIC" ProductionMeetingMinutes.tmp > ProductionMeetingMinutes.txt
+sudo sed '17i\'"   * $TOPIC" ProductionMeetingMinutes.tmp > ProductionMeetingMinutes.txt
 
 # Corrects permissions if needed
-#sudo chown apache:apache ProductionMeetingMinutes.txt
+sudo chown apache:apache ProductionMeetingMinutes.txt
 
 # Places edited file into production
-#sudo mv ProductionMeetingMinutes.txt /usr/local/twiki/data/Operations/
+sudo mv ProductionMeetingMinutes.txt /usr/local/twiki/data/Operations/
